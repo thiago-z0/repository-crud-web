26da9cc5d7e88a2d2c412d587f7eb8e2deb7d082
Thiago Ribeiro Correia <thiago@MacBook-Air-de-Thiago.local>
2020-04-13 12:25:37 -0300

    desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
+import api from './services/api'
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()  => {
+      getRepositories()
+    }, []);
+
+    async function getRepositories() {
+      api.get('/repositories').then(response => {
+        setRepositories(response.data)
+      })
+    }
+
   async function handleAddRepository() {
-    // TODO
+    
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete('/repositories/'+id).then(response => {
+      getRepositories()
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map( repository => 
+              <li key={repository.id}>{repository.title}
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>
+            )
+        }
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
